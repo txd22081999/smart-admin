ce0d4de74f44587f1431a8a668d961a61cbe6b1f
Duy Tran <txd22081999@gmail.com>
2021-06-25 18:02:34 +0700

    Map

--- src/views/app/map/map.js
+++ src/views/app/map/map.js
@@ -5,6 +5,7 @@ import IntlMessages from '../../../helpers/IntlMessages'
 import { Colxx, Separator } from '../../../components/common/CustomBootstrap'
 import Breadcrumb from '../../../containers/navs/Breadcrumb'
 import Pusher from 'pusher-js'
+import { getRestaurant } from 'src/redux/actions'
 
 import { YMaps, Map, Placemark } from 'react-yandex-maps'
 import {
@@ -15,54 +16,87 @@ import {
 } from 'react-google-maps'
 import { PUSHER_APP_CLUSTER, PUSHER_APP_KEY } from 'src/constants/config'
 import { listenNotification } from 'src/helpers/Utils'
+import { connect } from 'react-redux'
+import { useEffect } from 'react'
+
+// const MapWithAMarker = (props) => {
+//   console.log(props)
+//   return withScriptjs(
+//     withGoogleMap((props) => (
+//       <GoogleMap
+//         defaultZoom={8}
+//         defaultCenter={{ lat: 10.7944422, lng: 106.6671633 }}
+//       >
+//         {/* <Marker position={{ lat: -34.397, lng: 150.644 }} /> */}
+//         {/* 10.7944422,106.6671633,17z */}
+//         <Marker position={{ lat: 10.7944422, lng: 106.6671633 }} />
+//       </GoogleMap>
+//     ))
+//   )
+// }
 
 const MapWithAMarker = withScriptjs(
-  withGoogleMap((props) => (
-    <GoogleMap
-      defaultZoom={8}
-      defaultCenter={{ lat: 10.7944422, lng: 106.6671633 }}
-    >
-      {/* <Marker position={{ lat: -34.397, lng: 150.644 }} /> */}
-      {/* 10.7944422,106.6671633,17z */}
-      <Marker position={{ lat: 10.7944422, lng: 106.6671633 }} />
-    </GoogleMap>
-  ))
+  withGoogleMap((props) => {
+    console.log(props)
+    return (
+      <GoogleMap
+        defaultZoom={15}
+        defaultCenter={{ lat: 10.7944422, lng: 106.6671633 }}
+      >
+        {/* <Marker position={{ lat: -34.397, lng: 150.644 }} /> */}
+        {/* 10.7944422,106.6671633,17z */}
+        <Marker position={{ lat: 10.7944422, lng: 106.6671633 }} />
+      </GoogleMap>
+    )
+  })
 )
 
-export default class MapsUi extends Component {
-  componentDidMount() {
+const MapsUi = (props) => {
+  const { match, getRestaurant, restaurant } = props
+
+  useEffect(() => {
     console.log('DID MOUNT')
-    // const test_restaurant = `59648039-fb38-4a5a-8ce7-6938b27b76ab`
-    // const restaurantId = localStorage.getItem('restaurant_id')
+    console.log(props)
+    const restaurantId = localStorage.getItem('restaurant_id')
+    const merchantId = localStorage.getItem('merchant_id')
+
+    getRestaurant(merchantId, restaurantId)
     listenNotification()
+  }, [])
+
+  const { position } = restaurant
+
+  if (!position) {
+    return <div className='loading'></div>
   }
 
-  render() {
-    return (
-      <Fragment>
-        <Row>
-          <Colxx xxs='12'>
-            <Breadcrumb heading='menu.maps' match={this.props.match} />
-            <Separator className='mb-5' />
-          </Colxx>
-        </Row>
-        <Row>
-          <Colxx xxs='12'>
-            <Card className='mb-4'>
-              <CardBody>
-                <CardTitle>
-                  <IntlMessages id='maps.google' />
-                </CardTitle>
-                <MapWithAMarker
-                  googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyCO8MfadmlotuuHC8wmjwL_46I5QAMIiRU&v=3.exp&libraries=geometry,drawing,places'
-                  //   googleMapURL='https://goo.gl/maps/XJVJnLUGwVTjLYpt8'
-                  loadingElement={<div className='map-item' />}
-                  containerElement={<div className='map-item' />}
-                  mapElement={<div className='map-item' />}
-                />
-              </CardBody>
-            </Card>
-            {/* 
+  return (
+    <Fragment>
+      <Row>
+        <Colxx xxs='12'>
+          <Breadcrumb heading='menu.maps' match={match} />
+          <Separator className='mb-5' />
+        </Colxx>
+      </Row>
+      <Row>
+        <Colxx xxs='12'>
+          <Card className='mb-4'>
+            <CardBody>
+              <CardTitle>
+                <IntlMessages id='maps.google' />
+              </CardTitle>
+              <MapWithAMarker
+                googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyCO8MfadmlotuuHC8wmjwL_46I5QAMIiRU&v=3.exp&libraries=geometry,drawing,places'
+                //   googleMapURL='https://goo.gl/maps/XJVJnLUGwVTjLYpt8'
+                loadingElement={<div className='map-item' />}
+                containerElement={<div className='map-item' />}
+                mapElement={<div className='map-item' />}
+                lat={position.latitude}
+                lng={position.longitude}
+              />
+            </CardBody>
+          </Card>
+          {/* 
             <Card className='mb-4'>
               <CardBody>
                 <CardTitle>
@@ -80,9 +114,18 @@ export default class MapsUi extends Component {
                 </div>
               </CardBody>
             </Card> */}
-          </Colxx>
-        </Row>
-      </Fragment>
-    )
+        </Colxx>
+      </Row>
+    </Fragment>
+  )
+}
+
+const mapStateToProps = ({ restaurantInfo: { restaurant } }) => {
+  return {
+    restaurant,
   }
 }
+
+export default connect(mapStateToProps, {
+  getRestaurant,
+})(MapsUi)
